ba4853b77b19bd50b0c2217f480dc02f9c8599b1
Richard Marquez <rmarquez14@winona.edu>
2016-02-07 16:41:50 -0600

    made config more generic

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var http = require('http'),
     proxy = httpProxy.createProxyServer({}),
     config = require('./config.json');
 
+// Retrieve user configs
 var port = Number(config.proxyPort);
-
 var domains = config.domains;
 var paths = config.paths;
 
@@ -15,14 +15,17 @@ http.createServer(function(req, res) {
   var hostname = req.headers.host.split(":")[0];
   var pathname = url.parse(req.url).pathname;
 
+  // Attempt to recognize domain
   if (domains[hostname]) {
     appPort = domains[hostname];
   }
 
+  // Attempt to recognize path
   if (paths[pathname]) {
     appPort = paths[pathname];
   }
 
+  // Proxy to appropriate port for application
   proxy.web(req, res, { target: 'http://localhost:' + appPort });
 
 }).listen(port, function() {
